4e8f2cbc5024799ec66ff36f5769e043f24fee23
neeta <neetapurohit56@gmail.com>
2026-07-26 15:40:57 +0530

    user.model.js created

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating a user"],
+      trim: true,
+      lowercase: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email address"],
+      unique: true,
+    },
+
+    name: {
+      type: String,
+      required: [true, "Name is required for creating an account."],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required for creating an account."],
+      minlength: [6, "Password should contain more than 6 characters."],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash; //save that hashed password in db
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password); //it'll give true false based on password
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
